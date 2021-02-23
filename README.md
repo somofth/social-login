0f60b634488b056b8c8b9b8c3b6ca7f5dcaa7d98
doyle <il4479@hanmail.net>
2021-02-23 13:52:00 +0900

    read

---
+++ README.md
@@ -0,0 +1,8 @@
+# FAST API
+
+파이썬으로 NodeJs(ExpressJS)를 사용하는 기분이란 ?!
+
+## How to ?
+- pip3 install fastapi
+- pip3 install uvicorn
+- uvicorn main:app --reload
---
+++ main.py
@@ -0,0 +1,18 @@
+# pip3 install fastapi
+# pip3 install uvicorn
+
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
